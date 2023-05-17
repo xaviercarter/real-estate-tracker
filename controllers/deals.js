a1f6b5a296714d8c43fb3915ca190f878651267d
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-05-17 14:36:14 -0400

    add index routes

--- controllers/deals.js
+++ controllers/deals.js
@@ -13,8 +13,55 @@ function newDeal(req, res) {
     res.render('deals/new');
 };
 
+function create (req, res) {
+    console.log('this is the req.body in create\n', req.body);
+    
+    //remove white space from strings
+    // req.body.cast = req.body.cast.replace(/\s*, \s*g, ','/);
+    // req.body.cast = req.body.cast.split(',');
+    // console.log('req.body.cast halfway through conversion\n', req.body.cast);
+    // console.log('req.body after data conversion\n', req.body);
 
+    Deal.create(req.body)
+        .then(dealDoc => {
+            console.log('=== below is the deal from the db ===');
+            console.log(dealDoc);
+            console.log('===================');
+        
+            return res.send(`Deal Created: ${dealDoc.title}`);
+    })
+    
+    
+        .catch(err => {
+            console.log('============err');
+            console.log(err);
+            console.log('============');
+        
+            return res.send('err creating, check the terminal');
+    })
+    
+    
+    // res.send('hit the post route for deals');
+}
 
+// find all movies and send back as response
+function index(req, res) {
+
+    Deal.find({})
+        .then(dealDocs => {
+            console.log('found all deals\n', dealDocs)
+            res.render('deals/index', { deals: dealDocs })
+        })
+
+        .catch(err => {
+            console.log('============err');
+            console.log(err);
+            console.log('============');
+        
+            return res.send('err creating, check the terminal');
+        })
+
+}
 
 
 
@@ -24,4 +71,6 @@ function newDeal(req, res) {
 ///////////////////////
 module.exports = {
     new: newDeal,
+    create,
+    index,
 };
--- routes/deals.js
+++ routes/deals.js
@@ -7,12 +7,19 @@ const router = express.Router();
 const dealCtrl = require('../controllers/deals');
 
 
+
 /////////////////////////////
 // define router functions //
 /////////////////////////////
 
 /* GET deals create form. */
 router.get('/new', dealCtrl.new);
+
+//GET all deals
+router.get('/', dealCtrl.index);
+
+// post deal creation
+router.post('/', dealCtrl.create);
 
 
 
